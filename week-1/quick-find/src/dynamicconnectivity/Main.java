4acf0496565d24ee0902b5e9fe3848349d3d43fe
Rajan Chauhan <rajan.chauhan@ibm.com>
2023-07-28 21:43:37 +0530

    implementation of quick find algo for dynamic connectivity problem

---
+++ week-1/quick-find/src/dynamicconnectivity/Main.java
@@ -0,0 +1,62 @@
+package dynamicconnectivity;
+
+import java.util.Scanner;
+
+/**
+ * @author Rajan Chauhan
+ * @version 1.0
+ * @since 28/08/2023
+ * <p>client class to test {@link QuickFind}</p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        QuickFind quickFind = new QuickFind(5);
+        //first check if all objects are connected in array if not ask for objets to connect
+        while (!isAllConnected(quickFind)) {
+            System.out.println("\n\nObjects not fully connected to \nenter number to check if they are connected\n" +
+                    "if not then it will get connected using union\n");
+
+            int p, q;
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("enter value of p.");
+            p = sc.nextInt();
+            System.out.println("enter value of q.");
+            q = sc.nextInt();
+
+            //check if supplied numbers falls under the range
+            if (isObjectValid(p, q, quickFind)) {
+                System.out.printf("finding if %s and %s are connected objects.%n%n", p, q);
+
+                if (quickFind.find(p, q)) {
+                    System.out.printf("%s and %s are connected objects", p, q);
+                } else {
+                    System.out.printf("connecting objects %s and %s", p, q);
+                    quickFind.union(p, q);
+                }
+                quickFind.showItems();
+            } else {
+                System.out.printf("please check if %s OR %s are valid objects. Objects should be less then %s ", p, q, quickFind.numberOfObjects);
+            }
+        }
+        System.out.println("CONGRATULATION ! ALL OBJECTS CONNECTED...");
+
+    }
+
+    public static boolean isObjectValid(int p, int q, QuickFind quickFind) {
+        return p < quickFind.numberOfObjects && q < quickFind.numberOfObjects;
+    }
+
+    public static boolean isAllConnected(QuickFind quickFind) {
+        boolean result = true;
+        int connectingIndex = quickFind.arr[0];
+        for (int i = 1; i < quickFind.numberOfObjects; i++) {
+            if (quickFind.arr[i] != connectingIndex) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ week-1/quick-find/src/dynamicconnectivity/QuickFind.java
@@ -0,0 +1,78 @@
+package dynamicconnectivity;
+
+/**
+ * @author Rajan Chauhan
+ * @version 1.0
+ * @since 28/07/2023</p>
+ * <b><i>{@link QuickFind} </i></b>QuickFind algorithm class will implement an array based
+ * data structure that will have n number of objects, index of the array
+ * will mimic as object whereas the actual value will mimic as index.
+ * values will be equalized to connect all the objects.
+ */
+public class QuickFind {
+    int[] arr;
+    int numberOfObjects;
+
+    int pIndex;
+    int qIndex;
+
+    /**
+     * Constructor for {@link QuickFind}
+     *
+     * @param n number of objects
+     */
+    public QuickFind(int n) {
+        numberOfObjects = n;
+        arr = new int[numberOfObjects];
+        // n Array access - set id of each object to itself
+        for (int i = 0; i < numberOfObjects; i++) {
+            arr[i] = i;
+        }
+        showItems();
+    }
+
+    /**
+     * <b>showItems</b> method to show objets with their indexes
+     */
+    void showItems() {
+        System.out.println("\nObject state\n");
+        for (int i = 0; i < numberOfObjects; i++) {
+            System.out.print("|" + arr[i]);
+        }
+        System.out.print("|\n");
+        for (int i = 0; i < numberOfObjects; i++) {
+            System.out.print("|" + i);
+        }
+        System.out.print("|");
+    }
+
+    /**
+     * <b>find</b> method to check if two objects are connected
+     *
+     * @param p 1st object
+     * @param q 2nd object
+     * @return {@link Boolean} true if objects are connected and false if not.
+     */
+    boolean find(int p, int q) {
+        boolean findResult = false;
+        try {
+            //2 array access but constant
+            pIndex = arr[p];
+            qIndex = arr[q];
+
+            findResult = pIndex == qIndex;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("list does not contain the supplied objects");
+        }
+        return findResult;
+    }
+
+    void union(int p, int q) {
+        // 2N array access
+        for (int i = 0; i < numberOfObjects; i++) {
+            if (arr[i] == qIndex) {
+                arr[i] = pIndex;
+            }
+        }
+    }
+}
